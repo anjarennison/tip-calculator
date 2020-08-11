0f5170c1291e62da14b111100fc5da78c244e7f9
Anja <69499591+anjarennison@users.noreply.github.com>
2020-08-11 09:01:28 +0200

    Add files via upload

---
+++ tip-calculator/script.js
@@ -0,0 +1,13 @@
+document.getElementById('container').onchange = function() {
+    var bill = Number (document.getElementById('billTotal').value);
+    var tipPercent = document.getElementById('tipInput').value;
+    var split = document.getElementById('splitInput').value;
+    var tipValue = bill * (tipPercent / 100);
+    var newBillEach = (bill + tipValue) / split;
+    var tipEach = tipValue / split;
+
+    document.getElementById('tipOutput').innerHTML = tipPercent + "%";
+    document.getElementById('splitOutput').innerHTML = split;
+    document.getElementById('newBill').innerHTML = "DKK " + newBillEach.toFixed(2);
+    document.getElementById('tippp').innerHTML = "DKK " + tipEach.toFixed(2);
+}
